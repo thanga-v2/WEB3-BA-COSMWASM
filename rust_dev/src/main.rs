36d778bef76e3bc4ef7268d3d4e2ceaea19e48af
thangasblockchain <thangarajm@icloud.com>
2023-01-30 13:34:27 +0530

    adding workbook

---
+++ rust_dev/src/main.rs
@@ -0,0 +1,38 @@
+
+// from task 1
+
+// import the print.rs
+
+mod print;
+mod vars;
+mod types;
+mod strings;
+mod tuples;
+mod arrays;
+mod vectors;
+mod conditionals;
+mod loops;
+mod functions;
+fn main() {
+    // println!("Hello, Harshiv!");
+    // print::run();
+    // println!("below is workspace of variables");
+    // vars::run();
+    // println!("below is workspace of primitive data types");
+    // types::run();
+    // println!("below is workspace of strings");
+    // strings::run();
+    // println!("below is workspace of tuples");
+    // tuples::run();
+    // println!("below is workspace of arrays");
+    // arrays::run();
+    // println!("workspace for vectors");
+    // vectors::run();
+    // println!("workspace for conditionals");
+    // conditionals::run();
+    // println!("loops workspace");
+    // loops::run();
+    println!("functions workspace");
+    functions::run()
+
+}
